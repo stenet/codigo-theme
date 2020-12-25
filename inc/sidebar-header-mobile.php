1bbcb6127e1e157594e70bdaf484ba454262eb65
Stefan Heim <stefan.heim@hotmail.com>
2020-12-25 20:08:09 +0100

    Mobile Icon in Header anzeigen

--- inc/sidebar-header-mobile.php
+++ inc/sidebar-header-mobile.php
@@ -1,4 +1,5 @@
 <div id="nav-mobile-toggler" class="layout__nav__visiblity-flex">
+  <img src="<?php site_icon_url(40);?>" class="layout__nav-mobile__icon">
   <h1><?php echo get_bloginfo('name');?></h1>
   <i class="fas fa-bars"></i>
 </div>
