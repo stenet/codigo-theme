3683bfed4ebc4dbc0c16a59c0c1f8b656e023cc7
Stefan Heim <stefan.heim@hotmail.com>
2020-12-26 14:00:58 +0100

    Logo in Nav integriert

--- inc/sidebar-header.php
+++ inc/sidebar-header.php
@@ -1,6 +1,13 @@
 <div class="row">
   <div class="column">
-    <h1><?php echo get_bloginfo('name');?></h1>
+    <div class="layout__nav__icon__container">
+      <img src="<?php site_icon_url(300);?>" class="layout__nav__icon" alt>
+      <h1><?php echo get_bloginfo('name');?></h1>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="column">
     <div><?php get_social_media();?></div>
   </div>
 </div>
