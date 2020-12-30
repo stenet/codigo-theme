75174f488e9d16483269397adb9e695fc303b066
Stefan Heim <stefan.heim@hotmail.com>
2020-12-30 14:28:58 +0100

    Safari Icon Proportion korrigiert

--- inc/sidebar-icon.php
+++ inc/sidebar-icon.php
@@ -1,3 +1,3 @@
-<div class="lg:flex bottom-0 justify-center hidden">
-  <img src="<?php site_icon_url(200);?>" width="200" height="200" alt>
+<div class="lg:flex justify-center hidden">
+  <img src="<?php site_icon_url(200);?>" width="200" height="200" style="width:200px;height:200px" alt>
 </div>
