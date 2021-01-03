8d08b685abc76b1e59cea569cf51c79c867fec0a
Stefan Heim <stefan.heim@hotmail.com>
2021-01-03 11:01:16 +0100

    Anpassung Mobiles Menü

--- inc/sidebar-header-mobile.php
+++ inc/sidebar-header-mobile.php
@@ -1,11 +1,10 @@
-<div class="z-30 flex items-center flex-none px-4 py-2 text-xl nav-mobile bg-nav-bg text-nav-text lg:hidden">
-  <a href="/">
-    <i class="fas fa-home fa-fw"></i> 
-  </a>
-  <div class="flex-grow"></div>
+<div class="z-30 flex items-center flex-none px-4 py-2 text-2xl nav-mobile bg-nav-bg text-nav-text lg:hidden">
   <img src="<?php site_icon_url(50);?>" alt width="60" height="60">
   <div class="flex-grow"></div>
-  <a href="#">
+  <a href="/?s=">
+    <i class="fas fa-list-alt fa-fw"></i> 
+  </a>
+  <a class="ml-6" href="#">
     <i class="fas fa-bars fa-fw"></i>
   </a>
 </div>
