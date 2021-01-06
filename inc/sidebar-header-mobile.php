51996fea4aa3232a15fe165d8a7e8b4959ea123a
Stefan Heim <stefan.heim@hotmail.com>
2021-01-06 13:05:29 +0100

    Home-Icon in Mobiler Ansicht hinzugefügt

--- inc/sidebar-header-mobile.php
+++ inc/sidebar-header-mobile.php
@@ -1,7 +1,10 @@
 <div class="z-30 flex items-center flex-none px-4 py-2 text-2xl nav-mobile bg-nav-bg text-text lg:hidden">
   <img src="<?php site_icon_url(50);?>" alt width="60" height="60">
   <div class="flex-grow"></div>
-  <a href="/?s=">
+  <a href="/">
+    <i class="fas fa-home fa-fw"></i> 
+  </a>
+  <a class="ml-6" href="/?s=">
     <i class="fas fa-list-alt fa-fw"></i> 
   </a>
   <a class="ml-6" href="#">
