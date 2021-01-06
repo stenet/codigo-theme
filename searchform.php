152baa8b33a635e98c6379b3f4bd71ba76e391bc
Stefan Heim <stefan.heim@hotmail.com>
2021-01-06 13:03:17 +0100

    Outline in Search entfernt

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" class="flex flex-row overflow-hidden border rounded bg-input border-input-border focus-within:border-input-border-focus" action="/">
+<form role="search" method="get" id="searchform" class="flex flex-row overflow-hidden border rounded outline-none bg-input border-input-border focus-within:border-input-border-focus" action="/">
   <input class="flex-grow min-w-0 px-3 py-2 bg-transparent border-none rounded-none outline-none appearance-none" type="text" value="" name="s" id="s" placeholder="Suchen ...">
   <button class="flex-none px-3 py-2 bg-transparent appearance-none focus:outline-none text-text-a hover:text-text" type="submit">
     <i class="fas fa-search"></i>
