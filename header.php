5ac12ace98e57dc24edfe2149d5c71d9e237acca
Stefan Heim <stefan.heim@hotmail.com>
2021-01-08 12:49:07 +0100

    overflow-x-hidden bei mobiler Navigation

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
         <header class="sticky top-0 z-20 flex flex-col flex-none lg:w-80 lg:py-10 lg:min-h-screen lg:static lg:border-l lg:border-border bg-bg text-text">
           <?php include(get_template_directory() . '/inc/sidebar-header-mobile.php');?>
           <div class="nav-main__filler"></div>
-          <div class="flex-grow hidden px-4 py-4 -mt-12 space-y-8 overflow-scroll text-lg lg:mt-0 lg:space-y-8 lg:text-base lg:px-6 nav-main lg:block lg:flex-grow-0 lg:py-0 lg:overflow-auto">
+          <div class="flex-grow hidden px-4 py-4 -mt-12 space-y-8 overflow-scroll overflow-x-hidden text-lg lg:mt-0 lg:space-y-8 lg:text-base lg:px-6 nav-main lg:block lg:flex-grow-0 lg:py-0 lg:overflow-auto">
             <?php include(get_template_directory() . '/inc/sidebar-icon.php');?>
             <div class="hidden h-px bg-border lg:block"></div>
             <?php include(get_template_directory() . '/inc/sidebar-social.php');?>
