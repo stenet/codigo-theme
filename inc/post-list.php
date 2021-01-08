a66c86872bd2975b38d9d518a1304b27863aad8e
Stefan Heim <stefan.heim@hotmail.com>
2021-01-08 19:21:17 +0100

    Abstände zw. Posts in Auflistung verkleinert + Link in Header

--- inc/post-list.php
+++ inc/post-list.php
@@ -1,9 +1,9 @@
 <div class="post-list">
   <?php if (have_posts()) : while(have_posts()) : the_post();?>
   <article class="overflow-x-hidden">
-    <div class="mb-4 lg:flex">
+    <div class="mb-4 -mt-2 lg:flex">
       <div>
-        <h2><?php the_title();?></h2>
+        <a class="superplain" href="<?php the_permalink();?>"><h2><?php the_title();?></h2></a>
         <div class="-mt-4">
           <?php include(get_template_directory() . '/inc/post-date.php');?>
         </div>
@@ -19,7 +19,7 @@
       <a class="link-with-arrow" href="<?php the_permalink();?>">Jetzt lesen</i></a>
     </div>
 
-    <div class="h-px mt-8 bg-border"></div>
+    <div class="h-px mt-6 bg-border"></div>
   </article>
   <?php endwhile;?>
 
