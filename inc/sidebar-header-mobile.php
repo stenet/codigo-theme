ceed20dcd6df8c1d88492395ec2078470d353d7d
Stefan Heim <stefan.heim@hotmail.com>
2021-01-08 13:24:15 +0100

    Mobile Navigation farbig

--- inc/sidebar-header-mobile.php
+++ inc/sidebar-header-mobile.php
@@ -1,5 +1,5 @@
 <div class="z-30 flex items-center flex-none h-16 px-4 py-2 border-b nav-mobile bg-bg text-text border-border lg:hidden">
-  <img src="<?php echo get_template_directory_uri() . "/assets/logo_dark.png";?>" width="100" height="29" style="width:100px;height:29px" alt>
+  <img src="<?php echo get_template_directory_uri() . "/assets/logo.png";?>" width="100" height="29" style="width:100px;height:29px" alt>
   <div class="flex-grow"></div>
   <a href="/">
     <i class="fas fa-home fa-fw"></i> 
