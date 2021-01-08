aa61f0dec89c96b63996a94a1388e829058544fb
Stefan Heim <stefan.heim@hotmail.com>
2021-01-08 12:43:29 +0100

    Header in mobiler Nav größert

--- inc/sidebar-header-mobile.php
+++ inc/sidebar-header-mobile.php
@@ -1,4 +1,4 @@
-<div class="z-30 flex items-center flex-none px-4 py-2 border-b nav-mobile bg-bg text-text border-border lg:hidden">
+<div class="z-30 flex items-center flex-none h-16 px-4 py-2 border-b nav-mobile bg-bg text-text border-border lg:hidden">
   <img src="<?php echo get_template_directory_uri() . "/assets/logo_dark.png";?>" width="100" height="29" style="width:100px;height:29px" alt>
   <div class="flex-grow"></div>
   <a href="/">
